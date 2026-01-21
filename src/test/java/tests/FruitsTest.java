98955da73eb63f3714da1430f8112b6399c25b63
kulchub-Aleksandr <kulchub@gmail.com>
2026-01-21 21:24:48 +0500

    add test from lesson 2

---
+++ src/test/java/tests/FruitsTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import config.FruitsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FruitsTest {
+
+    @Test
+    public void testArray (){
+        System.setProperty("array","banana,apple,orange");
+        FruitsConfig config = ConfigFactory.create(FruitsConfig.class, System.getProperties());
+        assertThat(config.getFruitsArray())
+                .containsExactly("banana", "apple", "orange");
+    }
+
+    @Test
+    public void testList() {
+        System.setProperty("list", "apple,orange");
+
+        FruitsConfig config = ConfigFactory.create(FruitsConfig.class, System.getProperties());
+        assertThat(config.getFruitsList())
+                .containsExactly("apple", "orange");
+    }
+
+    @Test
+    public void testArrayWithDefaultValues() {
+        FruitsConfig config = ConfigFactory.create(FruitsConfig.class, System.getProperties());
+        assertThat(config.getFruitsArrayWithDefaultValues())
+                .containsExactly("orange", "apple");
+    }
+
+    @Test
+    public void testArrayWithSeparator() {
+        FruitsConfig config = ConfigFactory.create(FruitsConfig.class, System.getProperties());
+        assertThat(config.getFruitsListWithSeparator())
+                .containsExactly("orange", "banana");
+    }
+
+}
